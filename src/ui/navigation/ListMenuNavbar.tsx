24319d1b2f32d9503dd43959b3499b4a52ab0c68
arsham-ghaderi <arshamghaderi369@gmail.com>
2025-12-20 18:17:40 +0330

    add photo on project

--- src/ui/navigation/ListMenuNavbar.tsx
+++ src/ui/navigation/ListMenuNavbar.tsx
@@ -9,11 +9,11 @@ function ListMenuNavbar() {
         {
             NavbarListmenu.map((nav) => (
                 <li key={nav.id}>
-                    <Link href={nav.link} className='hamrah text-[30px] flex gap-1 hover:opacity-50 hover:scale-95 duration-300 cursor-pointer'>
+                    <Link href={nav.link} className='hamrah text-[22px] flex gap-1 hover:opacity-50 hover:scale-95 duration-300 cursor-pointer'>
                       {nav.title}
                       {
                         nav.icon &&
-                        <Image alt='...' src={nav.icon} className='w-[35px] h-[35px] mt-1'/>
+                        <Image alt='...' src={nav.icon} className='w-[25px] h-[25px] mt-1'/>
                       }
                     </Link>
                 </li>
